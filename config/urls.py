da7f4f2b7caa29d736d7e40a69adc89ce8271db6
Lei Hua <lhua@example.com>
2019-03-04 18:15:59 -0500

    blog home

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('about', include('about.urls'))
+    path('about/', include('about.urls')),
+    path('blog/', include('blog.urls')),
 ]
