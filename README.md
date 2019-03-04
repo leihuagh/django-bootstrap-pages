c45abf917e719a43d231e3d9b2af1dd8d2005773
Lei Hua <lhua@example.com>
2019-03-04 17:54:13 -0500

    add blog app

--- README.md
+++ README.md
@@ -13,3 +13,4 @@
 
 - core
 - about
+- blog
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
