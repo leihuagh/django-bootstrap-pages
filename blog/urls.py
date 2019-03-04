a3ce76a660b8007a03efe6fcf081cc1481004462
Lei Hua <lhua@example.com>
2019-03-04 18:40:01 -0500

    blog list

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, list_view
 
 app_name = 'blog'
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('list', list_view, name='list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html', {})
+
+
+def list_view(request):
+    return render(request, 'blog/list.html', {})
